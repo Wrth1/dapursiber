fc35f3cd32b03c0899783ef1b362eceef2436cb6
Wrth1 <billelimgg@gmail.com>
2024-10-19 23:26:25 +0700

    update db and models and readme

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 cara start:
+
 1. bikin .env pake .env.example
-2. run `php artisan migrate`
-3. ke laravel herd -> link existing project, pilih folder ini
+2. run `php artisan migrate:fresh`
+3. run `php artisan db:seed --class=RoleSeeder`
+4. run `php artisan db:seed --class=DatabaseSeeder`, ini buat bikin user dummy
+5. (optional) kalau malas run 3 3 nya, bisa run `php artisan migrate:fresh --seed`
+6. ke laravel herd -> link existing project, pilih folder ini
+7. enjoy http://dapursiber.test
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Roles;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +16,31 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Roles::factory()->create([
+            'role_name' => 'User',
         ]);
+
+        Roles::factory()->create([
+            'role_name' => 'Consultant',
+        ]);
+
+        Roles::factory()->create([
+            'role_name' => 'Admin',
+        ]);
+
+        $users = [
+            ['name' => 'User X', 'email' => 'alice@example.com', 'role_id' => 1],
+            ['name' => 'Consultant Y', 'email' => 'bob@example.com', 'role_id' => 2],
+            ['name' => 'Admin Z', 'email' => 'charlie@example.com', 'role_id' => 3],
+        ];
+
+        foreach ($users as $user) {
+            User::factory()->create($user);
+        }
     }
 }
